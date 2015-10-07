7e25ba4e1afb49112be278701f44e04dc3868da2
Retepred <peter.c.t.hyder@googlemail.com>
2015-10-07 17:11:44 +0100

    first

---
+++ randomnames.rb
@@ -0,0 +1,7 @@
+people = %w(Iwona Craig GraemeK Kathryn Kieran Peter Ere Chae Zsolt Simon Syed Neil GraemeS Keith Nevin)
+title = "<<<<<<<<<<<<<<<<<<<<<<<Random Name Generator>>>>>>>>>>>>>>>>>>>>> \n \n \n"
+title2 = "WELCOME TO PETERS WONDERFUL RANDOM NAME GENERATOR OF AWESOME \n \n \n"
+
+puts title.center(80)
+puts title2.center(80)
+
